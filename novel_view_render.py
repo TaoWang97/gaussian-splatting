e35f7eeb175d4cfdd7885046bfa70493c9b4e3ba
Patrick Wang <wangp@nvidia.com>
2025-06-07 16:05:07 -0700

    novel view rendering

---
+++ novel_view_render.py
@@ -0,0 +1,85 @@
+import torch
+import numpy as np
+from scene import Scene
+import os
+from tqdm import tqdm
+from os import makedirs
+from gaussian_renderer import render
+import torchvision
+from utils.general_utils import safe_state
+from argparse import ArgumentParser
+from arguments import ModelParams, PipelineParams, get_combined_args
+from gaussian_renderer import GaussianModel
+from scene.cameras import interpolate_camera
+try:
+    from diff_gaussian_rasterization import SparseGaussianAdam
+    SPARSE_ADAM_AVAILABLE = True
+except:
+    SPARSE_ADAM_AVAILABLE = False
+
+
+def render_set(model_path, name, iteration, views, gaussians, pipeline, background, train_test_exp, camera_name, separate_sh):
+    render_path = os.path.join(model_path, name, "ours_{}".format(iteration), camera_name)
+    frame_rate = 30
+    alphas = np.linspace(0.0, 1.0, frame_rate, endpoint=False)
+
+    makedirs(render_path, exist_ok=True)
+
+    # find the view from a certain camera
+    camera_list = [
+        cam for cam in views
+        if os.path.splitext(cam.image_name)[0].endswith(camera_name)
+    ]
+    camera_list.sort(key=lambda cam: cam.image_name)
+
+    novel_view = []
+    for i in range(len(camera_list) - 1):
+        ref_cam = camera_list[i]
+        tgt_cam = camera_list[i + 1]
+        for alpha in alphas:
+            novel_view.append(interpolate_camera(ref_cam, tgt_cam, alpha))
+
+
+    for idx, view in enumerate(tqdm(novel_view, desc="Rendering progress")):
+        rendering = render(view, gaussians, pipeline, background, use_trained_exp=train_test_exp, separate_sh=separate_sh)["render"]
+        gt = view.original_image[0:3, :, :]
+
+        if args.train_test_exp:
+            rendering = rendering[..., rendering.shape[-1] // 2:]
+            gt = gt[..., gt.shape[-1] // 2:]
+
+        torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
+
+def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, skip_train : bool, skip_test : bool, camera_name : str, separate_sh: bool):
+    with torch.no_grad():
+        gaussians = GaussianModel(dataset.sh_degree)
+        scene = Scene(dataset, gaussians, load_iteration=iteration, shuffle=False)
+
+        bg_color = [1,1,1] if dataset.white_background else [0, 0, 0]
+        background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
+
+        render_set(dataset.model_path, "novel_view", scene.loaded_iter, scene.getTrainCameras(), gaussians, pipeline, background, dataset.train_test_exp, camera_name, separate_sh)
+
+if __name__ == "__main__":
+    # Set up command line argument parser
+    parser = ArgumentParser(description="Testing script parameters")
+    model = ModelParams(parser, sentinel=True)
+    pipeline = PipelineParams(parser)
+    parser.add_argument("--iteration", default=-1, type=int)
+    parser.add_argument("--skip_train", action="store_true")
+    parser.add_argument("--skip_test", action="store_true")
+    parser.add_argument("--quiet", action="store_true")
+    parser.add_argument(
+        "--camera_name",
+        default="CAM_FRONT",
+        type=str,
+        choices=["CAM_FRONT","CAM_FRONT_LEFT","CAM_FRONT_LEFT","CAM_BACK", "CAM_BACK_LEFT", "CAM_BACK_RIGHT"],
+        help="Which camera to render"
+    )
+    args = get_combined_args(parser)
+    print("Rendering " + args.model_path)
+
+    # Initialize system state (RNG)
+    safe_state(args.quiet)
+
+    render_sets(model.extract(args), args.iteration, pipeline.extract(args), args.skip_train, args.skip_test, args.camera_name, SPARSE_ADAM_AVAILABLE)
